552147163a3a412c848c55a9a1816a9f71d1fa68
romintomasetti <romin.tomasetti@gmail.com>
2024-06-20 10:15:36 +0000

    atomics: insert op with atomic min

---
+++ include/kokkos-utils/atomics/InsertOp.hpp
@@ -0,0 +1,27 @@
+#ifndef KOKKOS_UTILS_ATOMICS_INSERTOP_HPP
+#define KOKKOS_UTILS_ATOMICS_INSERTOP_HPP
+
+#include <concepts>
+
+#include "kokkos-utils/concepts/View.hpp"
+
+namespace Kokkos::utils::atomics
+{
+
+/**
+ * @brief Insert an element in a view at a specific index using @c Kokkos::atomic_min.
+ *
+ * To be used with *e.g.* @c Kokkos::UnorderedMap::insert.
+ */
+struct InsertMin
+{
+    template <concepts::View ViewType, std::integral IndexType, typename ValueType>
+    KOKKOS_FUNCTION
+    void op(const ViewType& values, const IndexType index, ValueType&& value) const {
+        Kokkos::atomic_min(&values(index), std::forward<ValueType>(value));
+    }
+};
+
+} // namespace Kokkos::utils::atomics
+
+#endif // KOKKOS_UTILS_ATOMICS_INSERTOP_HPP
