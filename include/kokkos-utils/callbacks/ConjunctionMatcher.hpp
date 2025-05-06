b10f38bf0fd9eef71736c44b5ca7d90fed8fdef0
romintomasetti <romin.tomasetti@gmail.com>
2025-05-06 13:05:08 +0000

    callbacks: add a conjunction matcher

---
+++ include/kokkos-utils/callbacks/ConjunctionMatcher.hpp
@@ -0,0 +1,37 @@
+#ifndef KOKKOS_UTILS_CALLBACKS_CONJUNCTIONMATCHER_HPP
+#define KOKKOS_UTILS_CALLBACKS_CONJUNCTIONMATCHER_HPP
+
+#include "kokkos-utils/callbacks/Events.hpp"
+#include "kokkos-utils/callbacks/Matcher.hpp"
+
+namespace Kokkos::utils::callbacks
+{
+
+/**
+ * @brief Conjunction of matchers that is @c true only if all @ref matchers agree.
+ *
+ * @warning It is short-circuiting.
+ */
+template <Matcher... MatcherTypes> requires (sizeof...(MatcherTypes) > 1)
+struct ConjunctionMatcher
+{
+    template <typename... Args> requires (sizeof...(Args) == sizeof...(MatcherTypes))
+    explicit ConjunctionMatcher(Args&&... args) : matchers(std::forward<Args>(args)...) {}
+
+    template <Event EventType> requires (MatcherFor<MatcherTypes, EventType> && ...)
+    bool operator()(const EventType& event)
+    {
+        return std::apply([&event] (MatcherTypes&... matchers) {
+                return (matchers(event) && ...);
+        }, matchers);
+    }
+
+    std::tuple<MatcherTypes...> matchers;
+};
+
+template <typename... MatcherTypes>
+ConjunctionMatcher(MatcherTypes&&...) -> ConjunctionMatcher<MatcherTypes...>;
+
+} // namespace Kokkos::utils::callbacks
+
+#endif // KOKKOS_UTILS_CALLBACKS_CONJUNCTIONMATCHER_HPP
